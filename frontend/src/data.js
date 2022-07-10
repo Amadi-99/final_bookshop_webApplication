6765672181ee061c02c52cd6c9af6185f83442d1
Amadi-99 <96217@fhss.sjp.ac.lk>
2022-07-11 01:03:27 +0530

    add code for data js

---
+++ frontend/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://d1gbp99v447ls8.cloudfront.net/wp-content/uploads/2021/12/15110844/drug-crime-fiction.png",
+      title: "NEW PRODUCT",
+      desc: "Discover new books, reviews, and interviews with your favorite authors.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://d1gbp99v447ls8.cloudfront.net/wp-content/uploads/2021/12/15110844/drug-crime-fiction.png",
+      title: "FEATURED PRODUCT",
+      desc: "Discover new books, reviews, and interviews with your favorite authors.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://img.apmcdn.org/b2a6d421ce625968cdc6bdaa1f43f308a898ce8e/normal/cfced3-20161019-scaryreads.jpg",
+      title: "CLLECTIBLE BOOK",
+      desc: "Discover new books, reviews, and interviews with your favorite authors.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://assets.londonist.com/uploads/2012/03/i730/mayoralbiogs-300x225.jpg",
+      title: "Biography",
+    },
+    {
+      id: 2,
+      img: "https://di2ponv0v5otw.cloudfront.net/posts/2022/01/16/61e495642f3d192635bede39/m_61e49579ae766fc7bf40b7c9.jpg",
+      title: "Children",
+    },
+    {
+      id: 3,
+      img: "https://di2ponv0v5otw.cloudfront.net/posts/2022/01/04/61d4e81d463d4f2713d7e3c9/s_61d4e83609d760e7fe18244a.jpg",
+      title: "Fiction",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://static.wixstatic.com/media/ca5a16_e841557d534f416ea18f2248d52c7b6b~mv2.png/v1/fill/w_554,h_729,al_c,lg_1,q_90,enc_auto/whywait-mockup.png",
+    },
+    {
+      id:2,
+      img:"https://images-na.ssl-images-amazon.com/images/I/41BzklN%2BwWL._SR600%2C315_PIWhiteStrip%2CBottomLeft%2C0%2C35_SCLZZZZZZZ_FMpng_BG255%2C255%2C255.jpg",
+    },
+    {
+      id:3,
+      img:"https://images-na.ssl-images-amazon.com/images/I/714VnjKTvzL._AC_UL604_SR604,400_.jpg",
+    },
+    {
+      id:4,
+      img:"https://www.ims-online.com/images/books/Thomas_Maura_book2.png",
+    },
+    {
+      id:5,
+      img:"https://www.pngmart.com/files/8/Book-Cover-PNG-Clipart.png",
+    },
+    {
+      id:6,
+      img:"https://images-na.ssl-images-amazon.com/images/I/417BA65jFpL._SR600%2C315_PIWhiteStrip%2CBottomLeft%2C0%2C35_SCLZZZZZZZ_FMpng_BG255%2C255%2C255.jpg",
+    },
+    {
+      id:7,
+      img:"https://images-na.ssl-images-amazon.com/images/I/41aMh056w6L._SR600%2C315_PIWhiteStrip%2CBottomLeft%2C0%2C35_SCLZZZZZZZ_FMpng_BG255%2C255%2C255.jpg",
+    },
+    {
+      id:8,
+      img:"https://images-eu.ssl-images-amazon.com/images/I/416eyllg3XL._SR600%2C315_PIWhiteStrip%2CBottomLeft%2C0%2C35_SCLZZZZZZZ_FMpng_BG255%2C255%2C255.jpg",
+    },
+  ]
