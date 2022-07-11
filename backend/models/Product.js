72c30ead89b9181594a9bf2bbfe8e1b4935c3a86
Amadi-99 <96217@fhss.sjp.ac.lk>
2022-07-11 11:46:31 +0530

    code for cart and product in backend

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
-const { boolean } = require("webidl-conversions");
 
 const ProductSchema = new mongoose.Schema(
-    {
-      username: { type: String, required: true, unique: true },
-      email: { type: String, required: true, unique: true },
-      password: { type: String, required: true },
-      isAdmin: {
-        type: Boolean,
-        default: false,
-      },
-    },
-    { timestamps: true }
-  );
+  {
+    title: { type: String, required: true, unique: true },
+    desc: { type: String, required: true, },
+    img: { type: String, required: true },
+    categories: { type: Array },
+    size: { type: String },
+    price: { type: Number, required: true },
+    
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Product", ProductSchema);
